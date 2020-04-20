44f377e6c51454309afb9b6e48c8279414a4fde0
Atif Karbelkar <atif.krblkr@gmail.com>
2020-04-19 22:47:05 -0400

    Initial Commit

---
+++ CodeGladiators2020Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodeGladiators2020Cpp
+//
+//  Created by Atif Karbelkar on 19/04/20.
+//  Copyright © 2020 Karbelkar Designs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
